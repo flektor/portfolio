6604db78f05966fcf0024871bba3adabafebb457
flektor <gdrosin@gmail.com>
2023-08-11 11:36:34 +0200

    added about-me section

---
+++ src/components/about-me/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function AboutMe() {
+  return (
+    <div className="relative flex items-center">
+      <div className="w-full justify-center text-2xl">
+        <span className="text-5xl block mb-10">
+          About me
+        </span>
+        Hi, I'm George, a passionate Full-Stack Developer based in Berlin, armed
+        with a bachelor's degree in Informatics Engineering. I am in a constant
+        to improve my programming skills and bring innovative applications to
+        life. My recent accomplishments include successfully completing Spiced
+        Academy's intensive bootcamp, a transformative three-month experience
+        that has honed my expertise in web development. With a focus on
+        cutting-edge technologies such as React and Next.js, I am committed to
+        creating stunning web applications that seamlessly blend functionality
+        with creativity.
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,15 @@
 import Image from "next/image";
 import AvatarBubble from "~/components/avatar-bubble";
+import AboutMe from "~/components/about-me";
 
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen w-full items-center justify-between bg-[#27384c]">
       <section className="w-full max-w-5xl items-center justify-between font-mono text-sm">
         <div className="relative flex items-center">
-          <div className="w-full justify-center text-5xl pl-24">
-            <span className="text-5xl block mb-10">
-              Hello, I am George
-            </span>
-            A <strong>creative</strong>
+          <div className="w-full justify-center text-4xl pl-22">
+            Hi, I'm George. A creative
+
             <span className="text-8xl block -ml-2">
               Full-stack
             </span>
@@ -20,6 +19,9 @@ export default function Home() {
           </div>
           <AvatarBubble />
         </div>
+      </section>
+      <section className="w-full max-w-5xl items-center justify-between font-mono text-sm">
+        <AboutMe />
       </section>
     </main>
   );
