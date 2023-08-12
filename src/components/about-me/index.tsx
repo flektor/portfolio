d7eaca4e4b00c3ef6adbc7b574bfe2dd3c07c151
flektor <gdrosin@gmail.com>
2023-08-12 17:47:28 +0200

    updated about-me text

--- src/components/about-me/index.tsx
+++ src/components/about-me/index.tsx
@@ -7,15 +7,15 @@ export default function AboutMe() {
         <span className="text-5xl block mb-10">
           About me
         </span>
-        Hi, I'm George, a passionate Full-Stack Developer based in Berlin, armed
-        with a bachelor's degree in Informatics Engineering. I am in a constant
-        to improve my programming skills and bring innovative applications to
-        life. My recent accomplishments include successfully completing Spiced
-        Academy's intensive bootcamp, a transformative three-month experience
-        that has honed my expertise in web development. With a focus on
-        cutting-edge technologies such as React and Next.js, I am committed to
-        creating stunning web applications that seamlessly blend functionality
-        with creativity.
+        Hello, my name is George and I am a passionate Full-Stack Developer
+        based in Berlin. Equipped with a Bachelor's degree in Informatics
+        Engineering, I constantly strive to improve my programming skills and
+        bring innovative applications to life. My most recent achievement was
+        the successful completion of Spiced Academy's intensive bootcamp, a
+        transformative three-month experience that has honed my expertise in web
+        development. Committed to creating stunning web applications that
+        seamlessly blend functionality with creativity, my expertise lies in
+        cutting-edge technologies such as React, Next.js and more..
       </div>
     </div>
   );
