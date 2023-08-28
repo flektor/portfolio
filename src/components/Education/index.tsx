8fc2263522aef7dbe6ddc3d458558fc99190fe69
flektor <gdrosin@gmail.com>
2023-08-28 17:56:22 +0200

    changed education layout

--- src/components/Education/index.tsx
+++ src/components/Education/index.tsx
@@ -3,32 +3,40 @@ import React from "react";
 export default function Education() {
   return (
     <>
-      <div className="relative w-full flex items-center ">
+      <div className="relative w-full flex">
+        <span className="text-3xl -ml-4 md:-ml-11 mr-1 mt-1 ">
+          🎓
+        </span>
         <div className="relative justify-center">
-          <span className="text-4xl pb-1">
+          <span className="text-3xl md:text-4xl pb-1">
             Bachelor
           </span>
-          <span className="text-3xl p-1 ml-2">
-            in Informatics Engineering
+          <span className="whitespace-nowrap text-2xl md:text-3xl p-1 ml-2">
+            in <span className="block md:inline">Informatics Engineering</span>
           </span>
-          <span className="text-xl block">
+          <span className="-mt-6 md:m-0 md:text-xl block">
             Hellenic Mediterranean University
           </span>
-          <span className="text-xl text-white/60 pb-6 block">
+          <span className="md:text-xl text-white/60 pb-6 block  ">
             May 2021, Heraklion
           </span>
         </div>
       </div>
 
-      <div className="relative w-full flex items-center ">
+      <div className="relative w-full flex">
+        <span className="text-2xl -ml-4 md:-ml-10 mr-2 mt-1 ">
+          🌶️
+        </span>
         <div className="relative justify-center">
-          <span className="text-3xl pb-1">
-            Certificate in Full-Stack Web Development
+          <span className="text-2xl md:text-3xl pb-1">
+            Certificate in <span className="whitespace-nowrap">Full-Stack</span>
+            {" "}
+            <span className="whitespace-nowrap">Web Development</span>
           </span>
-          <span className="text-xl block">
+          <span className="md:text-xl block">
             SPICED Academy
           </span>
-          <span className="text-xl text-white/60 pb-6 block">
+          <span className="md:text-xl text-white/60 pb-6 block  ">
             Jul 2023, Berlin
           </span>
         </div>
