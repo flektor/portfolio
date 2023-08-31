fbd50ee7aace3f760d9a4240c7a681aeac78b269
flektor <gdrosin@gmail.com>
2023-09-01 00:02:14 +0200

    changed ' to &apos;

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -8,7 +8,7 @@ export default function AboutMe() {
         based in Berlin. Equipped with a Bachelor&apos;s degree in Informatics
         Engineering, I constantly strive to improve my programming skills and
         bring innovative applications to life. My most recent achievement was
-        the successful completion of Spiced Academy's intensive bootcamp, a
+        the successful completion of Spiced Academy&apos;s intensive bootcamp, a
         transformative three-month experience that has honed my expertise in web
         development. Committed to creating stunning web applications that
         seamlessly blend functionality with creativity, my expertise lies in
