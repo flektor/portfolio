5a213333aa071ab33f49df65fd09e07100829403
flektor <gdrosin@gmail.com>
2023-08-31 19:46:29 +0200

    catched error case, updated loading display

--- src/components/CodeWindow/index.tsx
+++ src/components/CodeWindow/index.tsx
@@ -23,37 +23,29 @@ export default function CodeWindow() {
     fetchData,
   );
 
-  const [hideComments, sethideComments] = useState(true);
+  const [hideComments, sethideComments] = useState(false);
 
-  function onhideCommentsHandler() {
+  function onHideCommentsHandler() {
     sethideComments((prev) => !prev);
   }
 
   if (error) return <div>failed to load</div>;
-  if (isLoading) return <div>loading...</div>;
-
-  if (!code) {
-    return <div>No data available</div>;
-  }
 
   return (
     <div className="max-w-5xl transition-colors ease-linear shadow-md">
       <div className="w-full h-12 rounded-t-lg bg-gray-200 dark:bg-gray-900 flex justify-start items-center space-x-1.5 px-4">
-        <span className="w-3 h-3 border-2 border-transparent dark:border-red-400 rounded-full bg-red-400 dark:bg-transparent ">
-        </span>
-        <span className="w-3 h-3 border-2 border-transparent dark:border-yellow-400 rounded-full bg-yellow-400 dark:bg-transparent">
-        </span>
-        <span className="w-3 h-3 border-2 border-transparent dark:border-green-400 rounded-full bg-green-400 dark:bg-transparent">
-        </span>
+        <span className="w-3 h-3 border-2 border-transparent dark:border-red-400 rounded-full bg-red-400 dark:bg-transparent" />
+        <span className="w-3 h-3 border-2 border-transparent dark:border-yellow-400 rounded-full bg-yellow-400 dark:bg-transparent" />
+        <span className="w-3 h-3 border-2 border-transparent dark:border-green-400 rounded-full bg-green-400 dark:bg-transparent" />
       </div>
 
       <div className="flex items-center p-2 bg-[#21252B] w-full">
         <input
           id="default-checkbox"
           type="checkbox"
-          checked={hideComments}
+          checked={!hideComments}
           className="ml-4 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-          onChange={onhideCommentsHandler}
+          onChange={onHideCommentsHandler}
         />
         <label
           htmlFor="default-checkbox"
@@ -64,13 +56,17 @@ export default function CodeWindow() {
       </div>
 
       <div className="dark:bg-[#21252B] border-t-0 w-full h-96 rounded-b-lg">
-        <SyntaxHighlighter
-          language="typescript"
-          showLineNumbers
-          style={hideComments ? customStyle : atomOneDark}
-        >
-          {code}
-        </SyntaxHighlighter>
+        {isLoading || typeof code !== "string"
+          ? "Loading file from github.."
+          : (
+            <SyntaxHighlighter
+              language="typescript"
+              showLineNumbers
+              style={hideComments ? customStyle : atomOneDark}
+            >
+              {code}
+            </SyntaxHighlighter>
+          )}
       </div>
     </div>
   );
